9f48d03190da5b3d33e5b5c381c2e406f0d6f05c
Rasmus Jørgensen <rbjoergensen@users.noreply.github.com>
2022-05-01 19:18:52 +0200

    Restructured and added some endpoints

--- main.go
+++ main.go
@@ -8,13 +8,39 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/", getHealth)
-	router.GET("/albums", getAlbums)
+	router.GET("/", getRoot)
+	router.GET("/health", getHealth)
+	router.GET("/is-alive", getAliveness)
+	router.GET("/api/v1/albums", getAlbums)
 
 	err := router.Run(":80")
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getRoot(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, rootObject)
+}
+
+type root struct {
+	Links rootLinks `json:"_Links"`
+}
+
+type rootLinks struct {
+	Health string `json:"Health"`
+	Alive  string `json:"Aliveness"`
+	Albums string `json:"Albums"`
+}
+
+var rootObject = root{Links: rootLinks{
+	Health: "https://api.callofthevoid.dk/health",
+	Alive:  "https://api.callofthevoid.dk/is-alive",
+	Albums: "https://api.callofthevoid.dk/api/v1/albums",
+}}
+
+func getAliveness(c *gin.Context) {
+	c.String(http.StatusOK, "alive")
 }
 
 func getHealth(c *gin.Context) {
